3df282334ba077b487050e499bbbc5599bc7abc2
Ivanov Igor <ivigraven@gmail.com>
2019-03-03 01:11:21 +0500

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,27 @@
+//--------task1----------
+let x = 2,
+	y = 22,
+	z = 0;
+	
+let result1 = x * y + 5 * z + x - 1;
+
+console.log(`Task1`); 
+console.log(`Result: ${result1}`); 
+
+//--------task2----------
+let algebra = 5,
+	geography = 4,
+	physics = 4;
+
+let result2 = (algebra + geography + physics) / 3;
+
+console.log(`Task2`); 
+console.log("Averge value: " + result2.toFixed(2)); 
+
+//--------task3----------
+let name = "Igor";
+
+let message = `Привет, мир! Меня зовут ${name}`;
+
+console.log(`Task3`); 
+console.log(message); 
